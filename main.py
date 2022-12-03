0c9bd18199faac9547ce82ddfec711ad5b2cfe4a
Abir <32369387+a-abir@users.noreply.github.com>
2022-12-02 20:47:36 -0500

    message test working

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ robo.setSpeed(.5)   # Set sim speed 50%
 
 robo.message("Hello World!")
 robo.paintWhite()   # put white brush down
+robo.message("Painting White")
 robo.left()         # turn left
 robo.left()         # turn left
 robo.pickUp()       # pick up cargo
+robo.message("Picked up cargo")
 robo.left()        # turn right
 robo.backward(10)    # move forward 10 steps
+robo.message("Moved forward 10 steps")
 robo.paintBlack()   # put black brush down
+robo.message("Painted black")
 robo.left()        # turn right
 robo.forward(3)    # move forward 10 steps
+robo.message("Moved forward 3 steps")
 robo.putDown()      # put down cargo
+robo.message("Put down cargo")
 robo.right(10)        # turn right
 
